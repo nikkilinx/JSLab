053d0638096f97e9a8d7edb48ca50bce2fdca43a
nikkilinx <nicole.l.lincoln@gmail.com>
2020-03-21 13:43:52 -0600

    add buttons

--- lab7.js
+++ lab7.js
@@ -5,22 +5,41 @@ const goalString = ["asdf;lkj", "lad fad sad dad",
 
 var i = 0;
 
+
 window.addEventListener("keypress", function (event) {
   const textbox = document.getElementById("userinput");
   textbox.textContent = textbox.textContent + event.key;
   if (textbox.textContent == goalString[i]) {
     const result = document.getElementById("result");
     result.textContent = "You Win!";
-    textbox.textContent = "";
-    i = i+1;
-    lvl = document.getElementById("level")
-    lvl.textContent = difficulties[i]
-    goal = document.getElementById("goal")
-    goal.textContent = goalString[i]
+    var next = document.createElement('button')
+    next.innerHTML = "NEXT"
+    document.body.appendChild(next)
+    next.addEventListener('click', function nextChallenge () {
+        document.body.removeChild(next);
+        textbox.textContent = "";
+        i = i+1;
+        lvl = document.getElementById("level")
+        lvl.textContent = difficulties[i]
+        goal = document.getElementById("goal")
+        goal.textContent = goalString[i]
+    })
   }
-})
+});
+
 
 function restart() {
   // In here set i=0
+  i = 0;
   // find the level, result, and goal elements and reset their textContent
+  goal = document.getElementById("goal");
+  goal.textContent = goalString[0];
+  lvl = document.getElementById("level");
+  lvl.textContent = difficulties[0];
+  const textbox = document.getElementById("userinput");
+  textbox.textContent = "";
+  document.body.removeChild(next);
 }
+
+restartButton = document.querySelector("#restartButton")
+restartButton.addEventListener('click', restart) // this is your restart func
